b312998d464a64a24005e4294892ecd71e900422
CODING-FTC-1\Valle <ValleyRobotics@outlook.com>
2024-10-31 19:24:48 -0500

    began coding encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderArm.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class EncoderArm {
+    public Servo gripper;
+    int gripperArmPixelPosition = 500;
+    int gripperArmHomePosition = 0;
+    int gripperArmHangPosition = 700;
 }
