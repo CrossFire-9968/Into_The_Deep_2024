a06b70df16587c94abc37047be4618a72f92dc76
DESKTOP-MSJPKQI\dawso <dawsonlydiag@gmail.com>
2024-11-03 14:50:17 -0600

    Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderArm.java
@@ -10,7 +10,38 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class EncoderArm {
     public Servo gripper;
-    int gripperArmPixelPosition = 500;
-    int gripperArmHomePosition = 0;
-    int gripperArmHangPosition = 700;
+    public DcMotor armMotor;
+    double armSpeedUp = 0.4;
+    double armSpeedDown = 0.2;
+//    int armMotorPosition = 500;
+//    int armMotorHomePosition = 0;
+//    int motorHangPosition = 700;
+
+    public void init (HardwareMap hwMap){
+        armMotor = hwMap.get(DcMotor.class, "armMotor");
+        armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        armMotor.setPower(0.0);
+    }
+
+    public int armControl(Gamepad gamepad) {
+        if (gamepad.a) {
+            armMotor.setPower(armSpeedDown);
+            armMotor.setTargetPosition(gripperArmHomePosition);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        else if (gamepad.y) {
+            armMotor.setPower(armSpeedUp);
+            armMotor.setTargetPosition(gripperArmPixelPosition);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        else if (gamepad.x) {
+            armMotor.setPower(armSpeedUp);
+            armMotor.setTargetPosition(gripperArmHangPosition);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        return armMotor.getCurrentPosition();
+    }
+
 }
