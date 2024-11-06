cf10ffdd596ad4a44a601384d055e8a3857f9921
DESKTOP-MSJPKQI\dawso <dawsonlydiag@gmail.com>
2024-11-05 19:24:30 -0600

    added telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorArm.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -17,11 +19,20 @@ public class ElevatorArm {
     }
     public void liftRaise(){
         elevator_Motor.setPower(0.6);
+
+        telemetry.addData("liftElevatorPosition", elevator_Motor);
+
     }
     public void liftLower() {
         elevator_Motor.setPower(-0.6);
+
+        telemetry.addData("liftElevatorPosition", elevator_Motor);
+
     }
     public void liftPowerOff(){
         elevator_Motor.setPower(0.0);
+
+        telemetry.addData("liftElevatorPosition", elevator_Motor);
     }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManual.java
@@ -37,5 +37,6 @@ public class RobotManual extends OpMode {
             telemetry.addLine("liftPowerOff");
         }
 
+
         telemetry.update();}
 }
